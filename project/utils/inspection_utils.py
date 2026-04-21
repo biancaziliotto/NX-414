6627fb6783e3a53d5cb2d5c09564da043f321fc7
biancaziliotto <bianca.ziliotto@epfl.ch>
2026-04-21 19:44:27 +0200

    inspection

--- project/utils/inspection_utils.py
+++ project/utils/inspection_utils.py
@@ -1,10 +1,21 @@
 import h5py
 
-def inspect(data, key="path"):
-    if not hasattr(data, "keys"):
-        print(f"{key} - {type(data)}")
-    else:
-        print("\t")
-        print(key)
+def inspect(data, key="path", level=0, verbose=False):
+    
+    if hasattr(data, "keys"):
+
+        print("  "*level, key, f" ({len(list(data.keys()))}) :", list(data.keys()))
+            
         for k in list(data.keys()):
-            inspect(data[k], k)
+            inspect(data[k], k, level+1, verbose=verbose)
+            
+    elif verbose:
+        print("  "*level, f"{key} - {data.shape}")
+
+    return
+
+def load_and_inspect_h5(path, verbose=False):
+    with h5py.File(path, "r") as f:
+        inspect(f, path, verbose=verbose)
+
+    return f
